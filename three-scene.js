5ff3a1d2bf82705524a68412015eaa93c1a117f7
vineethkrishnan1812-ai <vineethkrishnan1812@gmail.com>
2026-06-26 11:56:29 +0530

    Update three-scene.js

--- three-scene.js
+++ three-scene.js
@@ -195,10 +195,3 @@ body::before{
     );
     z-index:-1;
 }
-document.addEventListener("mousemove",(e)=>{
-
-document.body.style.setProperty("--x",e.clientX+"px");
-
-document.body.style.setProperty("--y",e.clientY+"px");
-
-});
