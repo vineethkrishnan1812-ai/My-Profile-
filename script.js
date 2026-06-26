4c5e1bf6e39a571c845d6d8d3dd856822e0d73fe
vineethkrishnan1812-ai <vineethkrishnan1812@gmail.com>
2026-06-26 11:47:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
 window.addEventListener("load", function () {
     document.getElementById("loader").style.display = "none";
 });
+const words=[
+"HR Professional",
+"Finance Graduate",
+"MBA Graduate",
+"Recruitment Specialist"
+];
+
+let i=0;
+
+let j=0;
+
+let current="";
+
+let deleting=false;
+
+function type(){
+
+const el=document.getElementById("typing");
+
+if(!el)return;
+
+if(!deleting){
+
+current=words[i].substring(0,j++);
+
+el.innerHTML=current;
+
+if(j>words[i].length){
+
+deleting=true;
+
+setTimeout(type,1500);
+
+return;
+
+}
+
+}else{
+
+current=words[i].substring(0,j--);
+
+el.innerHTML=current;
+
+if(j===0){
+
+deleting=false;
+
+i=(i+1)%words.length;
+
+}
+
+}
+
+setTimeout(type,deleting?50:120);
+
+}
+
+type();
